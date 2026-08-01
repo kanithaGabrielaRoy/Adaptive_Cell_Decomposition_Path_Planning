5dac48ca7a6a0a715924ba6dc860125c597210dc
Kanitha Gabriela Roy M <roykanitha@gmail.com>
2026-08-01 19:40:20 +0530

    Initial commit

--- build/acd_planner/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/kanitha/acd_ws/install/acd_planner'
--- install/acd_planner/lib/python3.10/site-packages/acd_planner/cell.py
+++
@@ -1,8 +0,0 @@
-class Cell:
-    def __init__(self, x, y, size):
-        self.x = x
-        self.y = y
-        self.size = size
-        self.is_obstacle = False
-        self.children = []
-
